95f3dd1719e77306a81e3f123162bc9476ff60a5
ecssiah <ecssiah@gmail.com>
2026-04-09 21:05:27 +0800

    python...

--- custom.el
+++ custom.el
@@ -5,7 +5,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(consult corfu dired-subtree eglot orderless vertico vterm)))
+   '(consult corfu dired-subtree eglot orderless pyvenv vertico vterm))
+ '(safe-local-variable-values
+   '((eval progn
+           (pyvenv-activate
+            (expand-file-name ".venv" (project-root (project-current))))
+           (eglot-ensure)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
